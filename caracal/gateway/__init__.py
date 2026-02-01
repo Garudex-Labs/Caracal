ecca6df6f31b4e39152f3b4dbf32b556d570a5cc
RAWx18 <rawx18.dev@gmail.com>
2026-02-02 00:04:33 +0530

    Create Authenticator service
    Signed-off-by: RAWx18 <rawx18.dev@gmail.com>

---
+++ caracal/gateway/__init__.py
@@ -0,0 +1,16 @@
+"""
+Gateway proxy module for Caracal Core v0.2.
+
+This module provides network-enforced policy enforcement through:
+- Authentication (mTLS, JWT, API keys)
+- Request interception and forwarding
+- Policy evaluation before API calls
+- Replay protection
+"""
+
+from caracal.gateway.auth import Authenticator, AuthenticationMethod
+
+__all__ = [
+    "Authenticator",
+    "AuthenticationMethod",
+]
