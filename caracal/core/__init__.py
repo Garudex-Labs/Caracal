8f8df19d99554a8b722a9c5eec05273235cba29c
RAWx18 <rawx18.dev@gmail.com>
2026-02-02 12:30:25 +0530

    Implement circuit breakers
    Signed-off-by: RAWx18 <rawx18.dev@gmail.com>

--- caracal/core/__init__.py
+++ caracal/core/__init__.py
@@ -7,4 +7,23 @@ This module contains the core primitives:
 - Ledger writer and query
 - Metering collector
 - Pricebook
+- Circuit breakers for resilience
 """
+
+from caracal.core.circuit_breaker import (
+    CircuitBreaker,
+    CircuitBreakerConfig,
+    CircuitBreakerError,
+    CircuitBreakerRegistry,
+    get_circuit_breaker,
+    get_circuit_breaker_registry,
+)
+
+__all__ = [
+    "CircuitBreaker",
+    "CircuitBreakerConfig",
+    "CircuitBreakerError",
+    "CircuitBreakerRegistry",
+    "get_circuit_breaker",
+    "get_circuit_breaker_registry",
+]
